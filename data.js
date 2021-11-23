ce65d7cd66901fa58195783bfff038dbb8935f39
SG <sonikagupta94@gmail.com>
2021-11-23 20:29:22 +0530

    Add Dummy Data

---
+++ data.js
@@ -0,0 +1,45 @@
+module.exports = {
+  cabs: [
+    {
+      id: 3,
+      drivername: 'Bohemian',
+      lat: 12.970118174497001,
+      lon: 77.63940372979536,
+      isBooked: false
+    },
+    {
+      id: 1,
+      drivername: 'Resida',
+      lat: 12.961482100489755,
+      lon: 77.64549770865008,
+      isBooked: false
+    },
+    {
+      id: 2,
+      drivername: 'Leela',
+      lat: 12.96080249358276,
+      lon: 77.64829793483432,
+      isBooked: false
+    }
+  ],
+  users: [
+    {
+      id: 1,
+      username: 'Geekskool',
+      lat: 12.961529724459048,
+      lon: 77.64430036382869
+    },
+    {
+      id: 2,
+      username: 'Ramada',
+      lat: 12.9555539710741,
+      lon: 77.64109328351748
+    },
+    {
+      id: 3,
+      username: 'Adobe',
+      lat: 12.925870035636654,
+      lon: 77.69337356700868
+    }
+  ]
+}
