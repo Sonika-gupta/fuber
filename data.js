e145e8957af779ce1f22134b10e78e6900fcdd60
Sonika Gupta <gupta.sonika103@gmail.com>
2021-11-24 11:30:07 +0530

    Update: Data.js

--- data.js
+++ data.js
@@ -5,41 +5,48 @@ module.exports = {
       drivername: 'Bohemian',
       lat: 12.970118174497001,
       lon: 77.63940372979536,
-      isBooked: false
+      isBooked: false,
+      isPink: true
     },
     {
       id: 1,
       drivername: 'Resida',
       lat: 12.961482100489755,
       lon: 77.64549770865008,
-      isBooked: false
+      isBooked: false,
+      isPink: false
     },
     {
       id: 2,
       drivername: 'Leela',
       lat: 12.96080249358276,
       lon: 77.64829793483432,
-      isBooked: false
+      isBooked: false,
+      isPink: false
     }
   ],
   users: [
     {
       id: 1,
-      username: 'Geekskool',
+      name: 'Geekskool',
       lat: 12.961529724459048,
-      lon: 77.64430036382869
+      lon: 77.64430036382869,
+      isRiding: false
     },
     {
       id: 2,
       username: 'Ramada',
       lat: 12.9555539710741,
-      lon: 77.64109328351748
+      lon: 77.64109328351748,
+      isRiding: false
     },
     {
       id: 3,
       username: 'Adobe',
       lat: 12.925870035636654,
-      lon: 77.69337356700868
+      lon: 77.69337356700868,
+      isRiding: false
     }
-  ]
+  ],
+  rides: []
 }
